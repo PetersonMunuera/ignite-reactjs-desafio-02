dfb33ae3e6fb598b159067bd9124b49226ed468d
Peterson Munuera <peterson.munuera@gmail.com>
2021-09-17 08:50:18 -0300

    desafio finalizado

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,36 @@
-export function SideBar() {
+import { Button } from './Button';
+
+import '../styles/sidebar.scss';
+
+type GenreProps = {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+type SidebarProps = {
+  genres: GenreProps[]
+  selectedGenreId: number
+  onClickButton: (genreId: number) => void
+}
+
+export function SideBar(props: SidebarProps) {
   // Complete aqui
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {props.genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.onClickButton(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
